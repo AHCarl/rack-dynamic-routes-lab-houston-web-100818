4133f5a7fd483875e98310fa9fc8fa6eccaf748c
Alexander Carlin <ahcarlin@gmail.com>
2018-10-30 20:11:15 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
     
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last 
-      item = Item.find
+      item = @@items.find{|i| i.name == item_name}
       if Item.all.include?(item)
         resp.write item.price
       else
